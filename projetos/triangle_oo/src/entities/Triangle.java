9d0154cde9bee1b5814b52e7e0ebf56428c27605
Rodolfo <r.ferrazmariucci@gmail.com>
2025-04-17 00:38:24 -0300

    new

---
+++ projetos/triangle_oo/src/entities/Triangle.java
@@ -0,0 +1,12 @@
+package entities;  // o tringulo é uma entidade do nosso negócio que mexe com áreas de triangulos
+
+public class Triangle {
+
+	public double a;
+	public double b;
+	public double c;
+
+}
+
+// aqui temos apenas atributos nessa classe, sem métodos
+	
