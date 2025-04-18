88bd0f4a3884335cfc8b56077a95603ec7d788cf
Rodolfo <r.ferrazmariucci@gmail.com>
2025-04-17 22:41:15 -0300

    add coments for study

--- triangle_oo/src/entities/Triangle.java
+++ triangle_oo/src/entities/Triangle.java
@@ -1,10 +1,14 @@
-package entities;  // o tringulo é uma entidade do nosso negócio que mexe com áreas de triangulos
+package entities;  // o triangulo é uma entidade do nosso negócio que mexe com áreas de triangulos
 
 public class Triangle {
 
 	public double a;
 	public double b;
 	public double c;
+	
+	
+	/* aqui abaixo em () ele nao  precisa preencher a lista de parametros do metodo,
+	 porque na classe ja tem tudo o que ele precisa, que é o A, B e C */
 	
 	public double area() {
 		double p = (a + b + c) / 2.0;
