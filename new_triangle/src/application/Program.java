2a7c0a7ddbacf0092437ead91b8d02f07fe47deb
Rodolfo <r.ferrazmariucci@gmail.com>
2025-04-26 11:50:38 -0300

    altered program

--- new_triangle/src/application/Program.java
+++ new_triangle/src/application/Program.java
@@ -6,20 +6,22 @@ import entities.Rectangle;
 
 public class Program {
     public static void main(String[] args) {
+    	
+     Locale.setDefault(Locale.US);
+     Scanner sc = new Scanner(System.in);
+     
+     Rectangle rectangle = new Rectangle();
+     
+     System.out.println("Enter rectangle width:");
+     rectangle.height = sc.nextDouble();
+     System.out.println("Enter rectangle height:");
+     rectangle.widght = sc.nextDouble();
+     
+     System.out.printf("AREA = %.2f%n", rectangle.area());
+     System.out.printf("PERIMETER = %.2f%n", rectangle.perimeter());
+     System.out.printf("DIAGONAL = %.2f%n", rectangle.diagonal());
+     
+     sc.close();
         
-        Locale.setDefault(Locale.US);
-        Scanner sc = new Scanner(System.in);
-        
-        Rectangle rectangle = new Rectangle();
-
-        System.out.println("Enter rectangle width and height:");
-        rectangle.width = sc.nextDouble();
-        rectangle.height = sc.nextDouble();
-
-        System.out.printf("AREA = %.2f%n", rectangle.area());
-        System.out.printf("PERIMETER = %.2f%n", rectangle.perimeter());
-        System.out.printf("DIAGONAL = %.2f%n", rectangle.diagonal());
-
-        sc.close();
     }
 }
--- new_triangle/src/entities/Rectangle.java
+++ new_triangle/src/entities/Rectangle.java
@@ -1,19 +1,24 @@
 package entities;
 
 public class Rectangle {
-    
-    public double width;
-    public double height;
-
-    public double area() {
-        return width * height;
-    }
-
-    public double perimeter() {
-        return 2 * (width + height);
-    }
-
-    public double diagonal() {
-        return Math.sqrt(width * width + height * height);
-    }
+	
+	public double widght;
+	public double height;
+	
+	public double area() {
+		return widght * height;
+	}
+	
+	public double perimeter() {
+		return 2 * (widght + height);
+	}
+	
+	public double diagonal() {
+		return Math.sqrt(widght * widght + height * height);
+	}
+	
+	
 }
+
+
+	
